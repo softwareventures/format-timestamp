49e756af2aa53dc275d25a0bb372f0b41eea8190
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-01 11:21:47 +0100

    feat(month): add month formatter

--- index.ts
+++ index.ts
@@ -68,3 +68,6 @@ export const shortYear = formatDate.shortYear;
 /** Formats the year portion of the specified Timestamp as a numeric string,
  * zero-padded to at least four digits. */
 export const year4 = formatDate.year4;
+
+/** Formats the month portion of the specified Timestamp as a numeric string. */
+export const month = formatDate.month;
