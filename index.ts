2fca6bdaafb4269d99574f5a3b733327e44c529b
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-05-01 10:50:31 +0100

    feat(timestampformatter): add TimestampFormatter type

---
+++ index.ts
@@ -0,0 +1,4 @@
+import {Timestamp} from "@softwareventures/timestamp";
+
+/** A function that formats a Timestamp or part of a Timestamp as a string. */
+export type TimestampFormatter = (timestamp: Timestamp) => string;
