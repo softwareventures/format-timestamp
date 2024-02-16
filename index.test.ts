480a9a96dedb35af45979d7cd9d87b25f5a5862a
Daniel Cassidy <mail@danielcassidy.me.uk>
2024-02-16 09:19:17 +0000

    test(iso8601): update for compatibility with @softwareventures/timestamp v2

--- index.test.ts
+++ index.test.ts
@@ -7,7 +7,7 @@ test("iso8601", t => {
         iso8601(timestamp({year: 2021, month: 5, day: 1, hours: 11, minutes: 58, seconds: 27.239})),
         "2021-05-01T11:58:27Z"
     );
-    t.is(iso8601(timestamp({year: 10000})), "10000-01-01T00:00:00Z");
+    t.is(iso8601(timestamp({year: 10000, month: 1, day: 1, hours: 0})), "10000-01-01T00:00:00Z");
     t.is(
         iso8601(timestamp({year: 1994, month: 11, day: 5, hours: 13, minutes: 15, seconds: 30})),
         "1994-11-05T13:15:30Z"
